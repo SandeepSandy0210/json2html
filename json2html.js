e35f0a5c974e1ccbc0326718a3e9c5924b9f1fc3
Sandy <psaisandeep2002@gmail.com>
2024-11-07 19:15:35 +0530

    first commit

---
+++ json2html.js
@@ -0,0 +1,31 @@
+// json2html.js
+
+export default function json2html(data) {
+    // Start the HTML table with data-user attribute
+    let html = `<table data-user="psaisandeep2002@gmail.com">`;
+    
+    // Extract column headers from the first data object keys
+    const headers = Object.keys(data.reduce((cols, row) => ({ ...cols, ...row }), {}));
+  
+    // Add the table headers
+    html += "<thead><tr>";
+    headers.forEach(header => {
+      html += `<th>${header}</th>`;
+    });
+    html += "</tr></thead>";
+  
+    // Add table rows
+    html += "<tbody>";
+    data.forEach(row => {
+      html += "<tr>";
+      headers.forEach(header => {
+        html += `<td>${row[header] || ""}</td>`; // Fill empty cells with empty strings
+      });
+      html += "</tr>";
+    });
+    html += "</tbody></table>";
+  
+    // Return the generated HTML string
+    return html;
+  }
+  
