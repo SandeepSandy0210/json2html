90f417ec3c3b333b0c6de5f68ba37f3413c37e49
Sai Sandeep <79784447+SandeepSandy0210@users.noreply.github.com>
2024-11-07 19:19:07 +0530

    Update json2html.js

--- json2html.js
+++ json2html.js
@@ -1,31 +1,30 @@
 // json2html.js
 
 export default function json2html(data) {
-    // Start the HTML table with data-user attribute
-    let html = `<table data-user="psaisandeep2002@gmail.com">`;
-    
-    // Extract column headers from the first data object keys
-    const headers = Object.keys(data.reduce((cols, row) => ({ ...cols, ...row }), {}));
+  // Start the HTML table with data-user attribute
+  let html = `<table data-user="psaisandeep2002@gmail.com">`;
   
-    // Add the table headers
-    html += "<thead><tr>";
+  // Extract column headers from all keys in the data
+  const headers = Object.keys(data.reduce((cols, row) => ({ ...cols, ...row }), {}));
+
+  // Add the table headers
+  html += "<thead><tr>";
+  headers.forEach(header => {
+    html += `<th>${header}</th>`;
+  });
+  html += "</tr></thead>";
+
+  // Add table rows
+  html += "<tbody>";
+  data.forEach(row => {
+    html += "<tr>";
     headers.forEach(header => {
-      html += `<th>${header}</th>`;
+      html += `<td>${row[header] || ""}</td>`;
     });
-    html += "</tr></thead>";
-  
-    // Add table rows
-    html += "<tbody>";
-    data.forEach(row => {
-      html += "<tr>";
-      headers.forEach(header => {
-        html += `<td>${row[header] || ""}</td>`; // Fill empty cells with empty strings
-      });
-      html += "</tr>";
-    });
-    html += "</tbody></table>";
-  
-    // Return the generated HTML string
-    return html;
-  }
-  
+    html += "</tr>";
+  });
+  html += "</tbody></table>";
+
+  // Return the generated HTML string
+  return html;
+}
